e3775fb66b36095b27a630959b05871a2929a4cc
Robert <rsmarincu@gmail.com>
2020-05-22 18:39:04 +0100

    initial

---
+++ src/math/math-api.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+import json
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('as', action='append')
+parser.add_argument('bs', action='append')
+
+
+class GetMessage(Resource):
+    def get(self):
+        return {"message":"Hello from the math endpoint"}
+
+class Add(Resource):
+    def post(self):
+        args = parser.parse_args()
+        to_return = [a + b for b in args['bs'] for a in args['as']]
+        return to_return
+
+class Subtract(Resource):
+    def post(self):
+        args = parser.parse_args()
+        to_return = [a - b for b in args['bs'] for a in args['as']]
+        return to_return
+
+
+api.add_resource(Add,'/math/add/')
+api.add_resource(Subtract,'/math/subtract/')
+
+api.add_resource(GetMessage,'/math/')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
---
+++ tests/test_sample.py
@@ -0,0 +1,9 @@
+import requests
+
+assert 1 == 1
+# def test_get_message():
+#     resp = requests.get('http://0.0.0.0:5000/')
+#     message = resp.json()
+#     assert resp.status_code == 200
+#     assert (message['message'] == "Hello from Azure")
+
