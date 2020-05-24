a987cefd86d2bcacaa7e3a95efc3ea918d7007b3
Robert <rsmarincu@gmail.com>
2020-05-24 12:11:39 +0100

    Travis test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,8 +1,2 @@
-
-assert 1 == 1
-# def test_get_message():
-#     resp = requests.get('http://0.0.0.0:5000/')
-#     message = resp.json()
-#     assert resp.status_code == 200
-#     assert (message['message'] == "Hello from Azure")
-
+def test_initial():
+    assert 1 == 1
